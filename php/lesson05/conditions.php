ec13a6f8c51f6d85725200ecc039d05497ae4342
lee-jiwon-dotcom <>
2026-05-08 16:04:11 +0900

    plus

--- php/lesson05/conditions.php
+++ php/lesson05/conditions.php
@@ -8,4 +8,58 @@ if ($score >= 80) {
 }   else {
     echo 'もう一度確認しましょう';
 }
-// PHP第5章の条件分岐の練習です
+?>
+
+//問題１。
+<?php
+$score = 75;
+
+if ($score >= 90) {
+    echo 'Aランクです。';
+} elseif ($score >= 70) {
+    echo 'Bランクです。';
+} elseif ($score >= 50) {
+    echo 'Cランクです。';
+}   else {
+    echo '再テストです。';
+}
+?>
+
+//問題２。
+<?php
+$age = 18;
+
+if ($age >= 20) {
+    echo '入場できます';
+}   elseif ($age >= 18) {
+    echo '保護者の同意が必要です。';
+}   else {
+    echo 'no enter';
+}
+?>
+
+//問題３。
+<?php
+$name = 'Yado';
+
+if ($name === ''){
+    echo '名前を入力してください';
+}   else {
+    echo 'こんにちは、' . $name . 'さん';
+}
+?>
+
+
+//問題４。
+<?php
+$isLoggedIn = true;
+$isAdmin = false;
+
+if ($isLoggedIn === true && $isAdmin === true) {
+    echo '管理者ページへようこそ';
+} elseif ($isLoggedIn === true && $isAdmin === false) {
+    echo 'マイページへようこそ';
+}   else {
+    echo 'ログインして';
+}
+?>
