e221f00bd75a0574aee98d68d9a42b64637289a3
lee-jiwon-dotcom <>
2026-05-08 17:22:58 +0900

    Hi

--- php/lesson06/loops.php
+++ php/lesson06/loops.php
@@ -8,3 +8,41 @@ $fruits = ['apple', 'banana', 'orange'];
 foreach ($fruits as $fruit) {
     echo $fruit . PHP_EOL;
 }
+
+//문제 1
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . PHP_EOL;
+}
+
+//문제 2
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 === 0) {
+        echo $i . PHP_EOL;
+    }
+}
+
+//문제 3
+$fruits = ['apple', 'banana', 'orange'];
+
+foreach ($fruits as $index => $fruit){
+    echo $fruit . PHP_EOL;
+}
+
+//문제 4
+$prices = [100, 250, 300];
+
+$total = 0;
+
+foreach ($prices as $price) {
+    $total = $total +$price;
+}   
+echo '금액은' . $total . '원입니다.' . PHP_EOL;
+    
+//문제 5
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . PHP_EOL;
+    
+    if ($i === 3) {
+        break;
+    }
+}
