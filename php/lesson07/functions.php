94d777c8050f7a1a7f30e4b004d423092592f389
lee-jiwon-dotcom <>
2026-05-08 18:10:10 +0900

    good

--- php/lesson07/functions.php
+++ php/lesson07/functions.php
@@ -6,3 +6,50 @@ function sumNumbers(int $left, int $right): int
 
 echo sumNumbers(3, 5);
 echo PHP_EOL;
+
+function multiplyNumbers(int $left, int $right): int
+{
+    return $left * $right;
+}
+
+echo multiplyNumbers(4, 6);
+echo PHP_EOL;
+
+function createGreeting(string $name): string
+{
+    return $name . "안녕하세요!";
+}
+
+echo createGreeting('Yado');
+echo PHP_EOL;
+
+function judgeScore(int $score): string
+{
+    if ($score >= 80) {
+        return "합격입니다.";
+    }
+    return "불합격 입니다.";
+}
+echo judgeScore(79);
+echo PHP_EOL;
+
+function addTax(int $price): int
+{
+    return $price * 1.1;
+}
+
+echo addTax(1000);  
+echo PHP_EOL;
+
+function sumPrices(array $prices): int
+{
+    $total = 0;
+
+    foreach ($prices as $price) {
+        $total = $total + $price;
+    }
+
+        return $total;
+}
+echo sumPrices([100, 200, 300]);
+echo PHP_EOL;
