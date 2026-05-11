b0b9d573514d7f451d5a7d330af93e6a332679cd
lee-jiwon-dotcom <>
2026-05-11 17:23:15 +0900

    修正しました。

--- php/lesson06/loops.php
+++ php/lesson06/loops.php
@@ -9,26 +9,26 @@ foreach ($fruits as $fruit) {
     echo $fruit . PHP_EOL;
 }
 
-//문제 1
+
 for ($i = 1; $i <= 10; $i++) {
     echo $i . PHP_EOL;
 }
 
-//문제 2
+
 for ($i = 1; $i <= 10; $i++) {
     if ($i % 2 === 0) {
         echo $i . PHP_EOL;
     }
 }
 
-//문제 3
+
 $fruits = ['apple', 'banana', 'orange'];
 
 foreach ($fruits as $index => $fruit){
     echo $fruit . PHP_EOL;
 }
 
-//문제 4
+
 $prices = [100, 250, 300];
 
 $total = 0;
@@ -36,9 +36,9 @@ $total = 0;
 foreach ($prices as $price) {
     $total = $total +$price;
 }   
-echo '금액은' . $total . '원입니다.' . PHP_EOL;
-    
-//문제 5
+echo '金額は' . $total . '円です。' . PHP_EOL;
+
+
 for ($i = 1; $i <= 10; $i++) {
     echo $i . PHP_EOL;
     
